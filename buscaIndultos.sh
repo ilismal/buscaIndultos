44727c949f8e25a2042e9eb5d0134331a9dd19d9
Ignacio Lis <ilismal@users.noreply.github.com>
2014-07-01 14:52:23 +0200

    Update buscaIndultos.sh

--- buscaIndultos.sh
+++ buscaIndultos.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 curl -s http://www.boe.es/robots.txt | grep "\.pdf" | grep -v \* | awk '{print $NF}' >> listaPDF.txt
+totalFicheros=$(cat listaPDF.txt | wc -l)
+echo "$totalFicheros ficheros a analizar"
+conIndulto=0
+sinIndulto=0
+contador=0
 for ruta in $(cat listaPDF.txt);
 do
+        echo -ne "Analizando a $contador de $totalFicheros [http://www.boe.es$ruta]"\\r
         wget http://www.boe.es$ruta -qO temp.pdf
         pdf2txt temp.pdf >> temp.txt 2>/dev/null
-        echo "$(cat temp.txt | sed 's/ //g' | grep -i indulto | wc -l) referencia(s) a indultos en http://www.boe.es$ruta"
+        referencias=$(cat temp.txt | sed 's/ //g' | grep -i "indult|conmuta" | wc -l)
+        if [ "$referencias" = "0" ]
+        then
+                sinIndulto=$(( sinIndulto + 1 ))
+        else
+                conIndulto=$(( conInduto + 1 ))
+                echo "http://www.boe.es$ruta" >> pdfConIndultos.txt
+        fi
+        #echo "$referencias referencia(s) a indultos en http://www.boe.es$ruta"
         rm temp.*
+        contador=$((contador + 1 ))
 done
+echo "$totalFicheros analizados"
+echo "$conIndulto con referencias a indultos"
+echo "$sinIndulto sin referencias a indultos"
